f261b462a1b0db39b1e0adff25c40b5c1d2ffc7f
SarahKinner <156657511+SarahKinner@users.noreply.github.com>
2026-02-26 01:28:02 -0700

    Made the vec lists needed to store the words 25 per level

---
+++ TypingPractice.rs
@@ -0,0 +1,37 @@
+//Typing Practice
+
+fn main() {
+    
+    let easy_words = vec![
+        "cat", "dog", "sun", "tree", "book",
+        "pen", "cup", "ball", "hat", "fish",
+        "car", "bird", "shoe", "milk", "apple",
+        "star", "leaf", "map", "egg", "door",
+        "toy", "ring", "cup", "bag", "rain"
+    ];
+
+    let medium_words = vec![
+        "keyboard", "window", "coffee", "pencil", "flower",
+        "orange", "garden", "butter", "laptop", "banana",
+        "bottle", "castle", "guitar", "friend", "letter",
+        "market", "pillow", "rocket", "school", "summer",
+        "travel", "violet", "wallet", "yellow", "zigzag"
+    ];
+
+    let hard_words = vec![
+        "ownership", "borrowing", "match", "exquisite", "programming",
+        "architecture", "development", "sophisticated", "assignment", "synchronize",
+        "functionality", "multiplication", "implementation", "interaction", "optimization",
+        "inheritance", "encapsulation", "polymorphism", "abstraction", "asynchronous",
+        "cryptography", "algorithm", "integration", "concurrency", "documentation"
+    ];
+
+
+
+
+}
+
+
+
+
+
